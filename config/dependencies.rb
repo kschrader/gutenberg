b6e90f098864149c796be26de395bedad055c736
kschrader <kurt@karmalab.org>
2008-12-02 14:27:45 -0800

    Fix the upgrade to actually work.

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,8 +1,8 @@
 # dependencies are generated using a strict version, don't forget to edit the dependency versions when upgrading.
-merb_gems_version = "1.0.2"
+merb_gems_version = "1.0.3"
 dm_gems_version   = "0.9.7"
 
-#dependency "merb-action-args", merb_gems_version
+dependency "merb-action-args", merb_gems_version
 dependency "merb-assets", merb_gems_version  
 dependency "merb-cache", merb_gems_version   
 dependency "merb-helpers", merb_gems_version 
@@ -20,6 +20,7 @@ dependency "dm-migrations", dm_gems_version
 dependency "dm-timestamps", dm_gems_version   
 dependency "dm-types", dm_gems_version
 dependency "dm-validations", dm_gems_version
+dependency "data_objects", "0.9.8"
 dependency "do_mysql", "0.9.8"
 dependency "will_paginate", "2.5.0"
 dependency "rdiscount", "1.2.11"
